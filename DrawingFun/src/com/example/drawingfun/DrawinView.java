59943b378b6925b224d4bd202e684d8d92ccf97f
pdrogfer <Pedro@192.168.1.100>
2013-10-19 11:00:34 +0200

    commit 1

---
+++ DrawingFun/src/com/example/drawingfun/DrawinView.java
@@ -0,0 +1,21 @@
+package com.example.drawingfun;
+
+import android.view.View;
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class DrawinView extends View {
+	// native variables
+	
+	// constructors
+	public DrawinView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		setupDrawin();
+	}
+	
+	// methods
+	private void setupDrawin() {
+		// get drawing area setup for interaction
+	}
+
+}
