cbe1ea15e1487e45fe3e70394b232b93bff2dd85
pdrogfer <Pedro@192.168.1.100>
2013-11-16 11:44:50 +0100

    New icons and french and spanish localization

--- DrawingFun/gen/com/example/drawingfun/R.java
+++ DrawingFun/gen/com/example/drawingfun/R.java
@@ -62,30 +62,29 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
-        public static final int brush=0x7f060004;
-        public static final int brush_size=0x7f06000f;
-        public static final int cancel=0x7f060015;
-        public static final int drawing_not_saved=0x7f060019;
-        public static final int drawing_saved=0x7f060018;
-        public static final int erase=0x7f060005;
-        public static final int eraser=0x7f060009;
-        public static final int eraser_size=0x7f060010;
-        public static final int hello_world=0x7f060002;
-        public static final int lrg=0x7f06000e;
-        public static final int med=0x7f06000d;
-        public static final int new_draw=0x7f06000b;
-        public static final int new_drawing=0x7f060011;
-        public static final int no=0x7f060014;
-        public static final int paint=0x7f060007;
-        public static final int pencil_edit=0x7f060008;
-        public static final int save=0x7f060006;
-        public static final int save_confirm=0x7f060017;
-        public static final int save_diskette_floppy_disk=0x7f06000a;
-        public static final int save_drawing=0x7f060016;
-        public static final int sml=0x7f06000c;
-        public static final int start_new=0x7f060003;
-        public static final int warning_new_drawing=0x7f060012;
-        public static final int yes=0x7f060013;
+        public static final int brush=0x7f060003;
+        public static final int brush_size=0x7f06000e;
+        public static final int cancel=0x7f060014;
+        public static final int drawing_not_saved=0x7f060018;
+        public static final int drawing_saved=0x7f060017;
+        public static final int erase=0x7f060004;
+        public static final int eraser=0x7f060008;
+        public static final int eraser_size=0x7f06000f;
+        public static final int lrg=0x7f06000d;
+        public static final int med=0x7f06000c;
+        public static final int new_draw=0x7f06000a;
+        public static final int new_drawing=0x7f060010;
+        public static final int no=0x7f060013;
+        public static final int paint=0x7f060006;
+        public static final int pencil_edit=0x7f060007;
+        public static final int save=0x7f060005;
+        public static final int save_confirm=0x7f060016;
+        public static final int save_diskette_floppy_disk=0x7f060009;
+        public static final int save_drawing=0x7f060015;
+        public static final int sml=0x7f06000b;
+        public static final int start_new=0x7f060002;
+        public static final int warning_new_drawing=0x7f060011;
+        public static final int yes=0x7f060012;
     }
     public static final class style {
         /** 
