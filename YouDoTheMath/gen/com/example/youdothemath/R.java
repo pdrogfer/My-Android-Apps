167afd868d61c2a549971779d5dd32be5524075b
pdrogfer <Pedro@192.168.1.100>
2013-11-23 20:28:23 +0100

    You Do The Math, interface and classes implemented. TODO refine colors. Logics.

--- YouDoTheMath/gen/com/example/youdothemath/R.java
+++ YouDoTheMath/gen/com/example/youdothemath/R.java
@@ -10,6 +10,9 @@ package com.example.youdothemath;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int colors=0x7f070000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -30,37 +33,44 @@ public final class R {
         public static final int yes=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f080013;
-        public static final int answer=0x7f080008;
-        public static final int btn1=0x7f080010;
-        public static final int btn2=0x7f080011;
-        public static final int btn3=0x7f080012;
-        public static final int btn4=0x7f08000d;
-        public static final int btn5=0x7f08000e;
-        public static final int btn6=0x7f08000f;
-        public static final int btn7=0x7f08000a;
-        public static final int btn8=0x7f08000b;
-        public static final int btn9=0x7f08000c;
-        public static final int clear=0x7f080009;
-        public static final int help_btn=0x7f080003;
-        public static final int high_btn=0x7f080004;
-        public static final int intro=0x7f080001;
-        public static final int operators=0x7f080000;
-        public static final int play_btn=0x7f080002;
-        public static final int question=0x7f080007;
-        public static final int response=0x7f080005;
-        public static final int score=0x7f080006;
+        public static final int action_settings=0x7f090017;
+        public static final int answer=0x7f09000a;
+        public static final int btn0=0x7f090015;
+        public static final int btn1=0x7f090012;
+        public static final int btn2=0x7f090013;
+        public static final int btn3=0x7f090014;
+        public static final int btn4=0x7f09000f;
+        public static final int btn5=0x7f090010;
+        public static final int btn6=0x7f090011;
+        public static final int btn7=0x7f09000c;
+        public static final int btn8=0x7f09000d;
+        public static final int btn9=0x7f09000e;
+        public static final int clear=0x7f09000b;
+        public static final int enter=0x7f090016;
+        public static final int help_btn=0x7f090005;
+        public static final int high_btn=0x7f090006;
+        public static final int high_head=0x7f090002;
+        public static final int high_scores_list=0x7f090003;
+        public static final int intro=0x7f090001;
+        public static final int operators=0x7f090000;
+        public static final int play_btn=0x7f090004;
+        public static final int question=0x7f090009;
+        public static final int response=0x7f090007;
+        public static final int score=0x7f090008;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_playgame=0x7f030001;
+        public static final int activity_high=0x7f030000;
+        public static final int activity_how=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_playgame=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int help_info=0x7f050002;
     }
     public static final class style {
         /** 
--- YouDoTheMath/src/com/example/youdothemath/MainActivity.java
+++ YouDoTheMath/src/com/example/youdothemath/MainActivity.java
@@ -1,32 +1,42 @@
 package com.example.youdothemath;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.app.Activity;
 import android.graphics.Typeface;
 import android.view.Menu;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
-	
+// TODO A–adir sonidos a los clicks y respuestas corectas e incorrectas
+public class MainActivity extends Activity implements OnClickListener {
+
 	TextView intro;
-	Button play, help;
+	private Button playBtn, helpBtn, highBtn;
 	Typeface myFont;
-	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		// all this to change "intro" font 
+
+		// all this to change "intro" font
 		intro = (TextView) findViewById(R.id.intro);
-		play = (Button) findViewById(R.id.play_btn);
-		help = (Button) findViewById(R.id.help_btn);
+		playBtn = (Button) findViewById(R.id.play_btn);
+		helpBtn = (Button) findViewById(R.id.help_btn);
+		highBtn = (Button) findViewById(R.id.high_btn);
 		Typeface myFont = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Thin.ttf");
 		intro.setTypeface(myFont);
-		play.setTypeface(myFont);
-		help.setTypeface(myFont);
+		playBtn.setTypeface(myFont);
+		helpBtn.setTypeface(myFont);
+		highBtn.setTypeface(myFont);
+
+		// listen to clicks
+		playBtn.setOnClickListener(this);
+		helpBtn.setOnClickListener(this);
+		highBtn.setOnClickListener(this);
+
 	}
 
 	@Override
@@ -34,9 +44,21 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-		
-	
-		
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		// respond to clicks
+		if (v.getId() == R.id.play_btn) {
+
+		}
+		if (v.getId() == R.id.help_btn) {
+
+		}
+		if (v.getId() == R.id.high_btn) {
+		}
+
 	}
 
 }
