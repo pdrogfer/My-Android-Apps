b76e8c9c6f703c87d0b61a10cb4f177547ccc134
pdrogfer <Pedro@192.168.1.100>
2013-12-03 17:48:59 +0100

    Operational. TODO Include alterations and kinds of intervals(perfect 5th. and so)

--- Intervalator2/src/com/example/intervalator2/MainActivity.java
+++ Intervalator2/src/com/example/intervalator2/MainActivity.java
@@ -9,17 +9,23 @@ import android.view.View.OnClickListener;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements OnClickListener {
+public class MainActivity extends Activity implements OnClickListener, OnCheckedChangeListener {
 
 	private Button noteStart, noteEnd, calculate, reset;
 	private String[] notes = { "C/Do", "D/Re", "E/Mi", "F/Fa", "G/Sol", "A/La", "B/Si" };
-	private int indNoteStart, indNoteEnd;
+	private int indNoteStart, indNoteEnd, intervalNum;
+	private int dir = 0;
 	private String interval;
 	private String[] intervals = { "Unison", "Second", "Third", "Fourth", "Fifth", "Sixth",
 			"Seventh" };
 	private TextView result;
+	private RadioGroup direction;
+	private RadioButton up, down;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +38,16 @@ public class MainActivity extends Activity implements OnClickListener {
 		calculate = (Button) findViewById(R.id.btnGo);
 		reset = (Button) findViewById(R.id.btnReset);
 		result = (TextView) findViewById(R.id.output);
+		direction = (RadioGroup) findViewById(R.id.rGrDirection);
+		up = (RadioButton) findViewById(R.id.btnDirUp);
+		down = (RadioButton) findViewById(R.id.btnDirDown);
 
 		// listen to clicks
 		noteStart.setOnClickListener(this);
 		noteEnd.setOnClickListener(this);
 		calculate.setOnClickListener(this);
 		reset.setOnClickListener(this);
-
+		direction.setOnCheckedChangeListener(this);
 	}
 
 	@Override
@@ -67,26 +76,49 @@ public class MainActivity extends Activity implements OnClickListener {
 								noteEnd.setText(notes[which]);
 								indNoteEnd = which;
 							}
-							
+
 						}
 					});
 			AlertDialog ad = builder.create();
 			ad.show();
-		}
-		else if (v.getId() == R.id.btnGo) {
+		} else if (v.getId() == R.id.btnGo) {
 			calculate();
-		}
-		else if (v.getId() == R.id.btnReset) {
+		} else if (v.getId() == R.id.btnReset) {
 			noteStart.setText(R.string.selNoteBtn);
 			noteEnd.setText(R.string.selNoteBtn);
 			result.setText(R.string.hintOutput);
 		}
-
+		//RadioButton dirChoice = (RadioButton) findViewById(direction.getCheckedRadioButtonId());
+		//dir = direction.getCheckedRadioButtonId();
 	}
 
 	protected void calculate() {
-		// TODO Calculate interval
-		interval = intervals[indNoteEnd - indNoteStart];
+		// Calculate interval
+		intervalNum = indNoteEnd - indNoteStart;
+		// Check direction and invert in case
+
+		if (dir == 1) {
+			intervalNum = 7 - intervalNum;
+		}
+		interval = intervals[intervalNum];
 		result.setText("Your interval is a " + interval);
 	}
+
+	@Override
+	public void onCheckedChanged(RadioGroup group, int checkedId) {
+		// Manage clicks in all radio buttons
+		if (group.getId() == R.id.rGrDirection) {
+			switch (checkedId) {
+			case R.id.btnDirUp:
+				dir = 0;
+				//result.setText("going up");
+				break;
+			case R.id.btnDirDown:
+				dir = 1;
+				//result.setText("going down");
+				break;
+			}
+		}
+	}
+
 }
